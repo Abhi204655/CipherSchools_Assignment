583c488dd40ba261563adb6be1e932bba1d807a2
abhi204655 <abhi9780253712@gmail.com>
2021-02-13 13:18:16 +0530

    renamed files according to the sequence

---
+++ Day1/5.platform-count.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPlatform(vector<float> &arrivalTime, vector<float> &departureTime)
+{
+}
+
+int main()
+{
+    vector<float> arrivalTime = {};
+    vector<float> departureTime = {};
+
+    cout << "The number of platforms required is : " << countPlatform(arrivalTime, departureTime) << endl;
+
+    return 0;
+}
