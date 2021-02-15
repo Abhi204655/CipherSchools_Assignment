13f0f13f2260b5fdb7839e876a2f531946b36e0d
abhi204655 <abhi9780253712@gmail.com>
2021-02-15 19:37:23 +0530

    Day-3 implemented linked list reverse

---
+++ 3-Day/1-linked_list.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int n)
+    {
+        this->data = n;
+        this->next = NULL;
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head = NULL;
+
+    LinkedList()
+    {
+        head = NULL;
+    }
+    LinkedList(int n)
+    {
+        head = new Node(n);
+    }
+    void AddNode(int n)
+    {
+        Node *node = new Node(n);
+        if (head == NULL)
+        {
+            head = node;
+        }
+        else
+        {
+            Node *cur = head;
+            while (cur->next != NULL)
+            {
+                cur = cur->next;
+            }
+            cur->next = node;
+        }
+    }
+    void PrintList()
+    {
+        Node *cur = head;
+        while (cur != NULL)
+        {
+            cout << cur->data << " -> ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+    void reverse()
+    {
+        Node *prev, *cur, *next;
+        prev = NULL;
+        cur = head;
+        next = NULL;
+
+        while (cur != NULL)
+        {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head = prev;
+    }
+};
+
+int main()
+{
+    LinkedList ll;
+    ll.AddNode(1);
+    ll.AddNode(2);
+    ll.AddNode(3);
+    ll.AddNode(4);
+    ll.AddNode(5);
+    ll.PrintList();
+    ll.reverse();
+    ll.PrintList();
+    return 0;
+}
